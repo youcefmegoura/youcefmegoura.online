ad01c3c0d450799b91e5f6a8d0d1c7a358e6e65d
youcefmegoura <meg.youcef@yahoo.fr>
2026-04-05 17:32:06 +0200

    fix: remove sentry sample page

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -4,8 +4,6 @@ export async function register() {
   if (process.env.NEXT_RUNTIME === "nodejs") {
     Sentry.init({
       dsn: "https://06981274071df08b0fe6f660214d2648@o4511149778075648.ingest.de.sentry.io/4511152583802960",
-
-      // Only active during build since this is a static export
       tracesSampleRate: 1,
     });
   }
@@ -13,7 +11,6 @@ export async function register() {
   if (process.env.NEXT_RUNTIME === "edge") {
     Sentry.init({
       dsn: "https://06981274071df08b0fe6f660214d2648@o4511149778075648.ingest.de.sentry.io/4511152583802960",
-
       tracesSampleRate: 1,
     });
   }
